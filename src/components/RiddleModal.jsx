aaa96c3bd2eb7bfbe33609c509210d77ddfc5577
hsavzyan <hsavzyan@gmail.com>
2023-05-20 21:59:49 +0200

    hint

--- src/components/RiddleModal.jsx
+++ src/components/RiddleModal.jsx
@@ -1,17 +1,37 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from "react";
 import "./RiddleModal.css";
 
 function RiddleModal({ riddle, handleUserChoice }) {
+  const [hintShown, setHintShown] = useState(false);
+
+  const handleHintClick = () => {
+    setHintShown(!hintShown);
+  };
+
   return (
     <div className="riddle-modal-parent">
       <div className="riddle-modal">
         <h2 className="riddle-question">{riddle.question}</h2>
-        <img
-          className="riddle-image"
-          src={riddle.image.src}
-          alt={riddle.image.alt}
-        />
+        <div className="riddle-image-container">
+          <img
+            className="riddle-image"
+            src={riddle.image.src}
+            alt={riddle.image.alt}
+          />
+          <button
+            className={`hint-btn ${hintShown ? "hint-shown" : ""}`}
+            onClick={handleHintClick}
+          >
+            {hintShown ? "Hide Hint" : "Hint"}
+          </button>
+          {hintShown && (
+            <div className="hint-text">
+              <p>{riddle.hint}</p>
+            </div>
+          )}
+        </div>
         <div className="choices-container">
           {riddle.choices.map(
             (choice, index) =>
